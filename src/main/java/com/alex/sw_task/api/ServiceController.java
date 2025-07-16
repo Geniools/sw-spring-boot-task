f9ca9a3830dbcf83eb087c75a0ff6525641fe1ba
Geniools <30665942+Geniools@users.noreply.github.com>
2025-07-16 22:18:18 +0200

    Added Cross-Origin support for frontend access

--- src/main/java/com/alex/sw_task/api/ServiceController.java
+++ src/main/java/com/alex/sw_task/api/ServiceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/services")
+@CrossOrigin(maxAge = 3600)
 public class ServiceController {
 	private final ServiceEntityService serviceEntityService;
 
